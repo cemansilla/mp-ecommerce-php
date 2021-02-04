ffe7e4a0bf88e4d805710be8b10aaf118f7b0d94
Cesar <mail.mansilla@gmail.com>
2021-02-03 22:54:55 -0300

    JSON list

--- ipn/index.php
+++ ipn/index.php
@@ -1,1 +1,11 @@
-<?php // Silence is gold ?>
+<?php
+$config = require(__DIR__ . '/../server/config.php');
+
+$files = glob(__DIR__ . "/*.json");
+
+foreach($files as $file){
+  $filename = basename($file);
+  echo sprintf("<a href='%s' target='_blank'>%s</a>", $config['base_url'] . $filename, $filename);
+  echo "<br>";
+}
+?>
