2678203798cd5390e202df3e5090b1c1aa4bdbb3
Cesar <mail.mansilla@gmail.com>
2021-02-03 22:44:25 -0300

    Ajustes

--- finish.php
+++ finish.php
@@ -5,21 +5,23 @@ switch($_GET['collection_status']){
   case "approved":
     $respuesta = array(
       'Payment' => $_GET['payment_id'],
-      'PaymentId' => $_GET['payment_method_id'],      
+      'PaymentId' => $_GET['payment_type'],      
       'ExternalReference' => $_GET['external_reference'],
       'Status' => $_GET['status'],
-      'MerchantOrder' => $_GET['merchant_order_id']        
+      'MerchantOrder' => $_GET['merchant_order_id']
     );
+
+    echo "Pago exitoso!<br>";
     echo json_encode($respuesta);
   break;
   
   case "pending":
-    echo "Pago pendiente por medio de pago offline";
+    echo "Pago pendiente por medio de pago offline<br>";
   break;
 
   case "failure":
   default:
-    echo "Pago rechazado o no finalizado";
+    echo "Pago rechazado o no finalizado<br>";
   break;            
 }
 
