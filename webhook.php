6593b00c5a7e4de94042f053f32fb9c7cbe6e615
Cesar <mail.mansilla@gmail.com>
2021-02-03 23:25:12 -0300

    Webhook fix

--- webhook.php
+++ webhook.php
@@ -1,6 +1,8 @@
 <?php
+$json = file_get_contents('php://input');
+
 $fp = fopen(__DIR__ . "/ipn/" . date('Y-m-d_H-i-s') . "-request.json", "w");
-fwrite($fp, $_REQUEST);
+fwrite($fp, $json);
 fclose($fp);
 
 header("HTTP/1.1 200 OK");
