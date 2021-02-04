69800601803752b39364cee0b09a9f48c650a0d4
Cesar <mail.mansilla@gmail.com>
2021-02-03 23:13:37 -0300

    Webhook

--- webhook.php
+++ webhook.php
@@ -1,12 +1,8 @@
 <?php
+$fp = fopen(__DIR__ . "/ipn/" . date('Y-m-d_H-i-s') . "-request.json", "w");
+fwrite($fp, $_REQUEST);
+fclose($fp);
+
 header("HTTP/1.1 200 OK");
 echo "HTTP/1.1 200 OK";
-
-$fp = fopen(__DIR__ . "/ipn/" . date('Y-m-d_H-i-s') . "-post.json", "w");
-fwrite($fp, $_POST);
-fclose($fp);
-
-$fp = fopen(__DIR__ . "/ipn/" . date('Y-m-d_H-i-s') . "-get.json", "w");
-fwrite($fp, $_GET);
-fclose($fp);
 ?>
